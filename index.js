300b280a995756b7544c3383c8a2d1c0e2764bb4
Cynthia Meli <cynthiagwyneth243@gmail.com>
2022-12-14 09:59:40 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function indexjs() {
+function cats (){
+    cats[2];
+    };
+}
+function ArrayFunctions(){
+    cats.length= 0;
+    cats.push("Milo", "Otis", "Garfield");
+    function destructivelyAppendCat(name){
+        cats.push(name);
+        cats;
+    }
+destructivelyAppendCat("Ralph");
+}
